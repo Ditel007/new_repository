e16464fa5c8d47e6baac1c27a6c8dc4f1dd3b6a6
Nikita Kislykh <kislykhnik@gmail.com>
2023-04-29 15:01:20 +0700

    grammar bug

--- str_func.py
+++ str_func.py
@@ -2,5 +2,5 @@ def str_funct(string):
     return string.upper()
 
 def str_title(string):
-    """функция переводит в верхний регистр первые буквы каждого слова"""
+    """Функция переводит в верхний регистр первые буквы каждого слова"""
     return string.title()
